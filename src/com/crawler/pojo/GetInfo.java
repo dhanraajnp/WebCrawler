4ae286e293d116e4854fa86543c9a69c3a0dae7f
Dhanraj <dhanraajnpatil@gmail.com>
2018-09-04 22:08:42 +0530

    Sample Pojo class

---
+++ src/com/crawler/pojo/GetInfo.java
@@ -0,0 +1,21 @@
+package com.crawler.controller;
+
+import org.jsoup.HttpStatusException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.net.UnknownHostException;
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class GetInfo {
+	
+	public String name;
+	public String passWord;
+	
+	
+	
+}
